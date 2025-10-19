ef75847520e675c7ea3148b807dfcb95cde2d10c
Taimur Ibrahim <tai.him18@gmail.com>
2025-10-19 21:01:22 +0200

    feat: added shakespeare dataloader

--- src/data/data.py
+++ src/data/data.py
@@ -1,8 +1,32 @@
-from datasets import load_dataset, load_dataset_builder
+from src.tokenizer import simpleTokenizer
+from torch import stack, randint
+import torch
+
+class shakespeareDataset:
+    def __init__(self, path="./src/data/shakespeare.txt", split_ratio: float = 0.9):
+        with open(path, "r") as f:
+            self.raw_text = f.read()
+            self.tokenizer = simpleTokenizer(self.raw_text) 
+            data = self.tokenizer.encode(self.raw_text)
+
+            n = int(split_ratio * len(self.raw_text))
+            self.train_data = data[:n]
+            self.test_data = data[n:]
+
+    def get_batch(self, split="train", ctx_len: int = 8, batch_size:int = 4):
+        data = self.train_data if split=="train" else self.test_data
+        idx = randint(len(data) - ctx_len, (batch_size,), )
+        x = stack([data[i: i + ctx_len] for i in idx])
+        y = stack([data[i + 1: i + ctx_len + 1] for i in idx])
+        
+        return x, y
 
 
-def load_dataset() -> str:
-    with open("input.txt", "r") as f:
-        text = (f.read())
+if __name__ == "__main__":
+    xb,yb = shakespeareDataset().get_batch()
+    print(xb.shape)
+    print(yb.shape)
 
-    return text
+    for i in range(4):
+        for l in range (8):
+            print(f"When input is {xb[i, :l+1].tolist()}, target is {yb[i, l]}")
--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,3 +1,4 @@
+import torch
 
 class simpleTokenizer():
     """Basic tokenize for character level encoding."""
@@ -7,11 +8,6 @@ class simpleTokenizer():
         self.ctoi = {c: idx for idx, c in enumerate(self.vocab)}
         self.itoc = {idx: c for idx, c in enumerate(self.vocab)}
 
-        self.encode = lambda string: [self.ctoi[char] for char in string]
+        self.encode = lambda string: torch.tensor([self.ctoi[char] for char in string])
         self.decode = lambda ints: "".join([self.itoc[i] for i in ints])
         
-    def encode(self, text: str) -> list[int]:
-        return self.encode(text)
-    
-    def decode(self, sequence: list[int]) -> str:
-        return self.decode(sequence)
