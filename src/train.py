705f15b15a5677b4efd1bcb5fbec7f4a04a771a1
Taimur Ibrahim <tai.him18@gmail.com>
2026-04-12 21:57:05 +0200

    define architecture

--- src/train.py
+++ src/train.py
@@ -5,12 +5,11 @@ from torch.nn import functional as F
 
 @dataclass
 class GPTConfig:
-    block_size: int = 256
-    vocab_size: int = 65
-    n_layer: int = 6
-    n_heads: int = 6
-    n_embd: int = 384
-
+    block_size: int = 1024
+    vocab_size: int = 50257
+    n_layer: int = 12
+    n_heads: int = 12
+    n_embd: int = 768
 
 class MLP(nn.Module):
     def __init__(self, config):
@@ -18,9 +17,42 @@ class MLP(nn.Module):
         self.gelu = nn.GELU(approximate='tanh')
         self.proj = nn.Linear(4 * config.n_embd, config.n_embd)
 
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.proj(x)
+        return x
 
 class CausalSelfAttention(nn.Module):
-    pass
+    def __init__(self, config):
+        super().__init__()
+        self.n_embd = config.n_embd
+        self.n_heads = config.n_heads
+
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd) 
+
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size)).view(1, 1, config.block_size, config.block_size))
+        
+    def forward(self, x):
+        B, T, C = x.size()
+
+        kqv = self.c_attn(x)
+        k, q, v = kqv.split(config.n_embd, dim=2)
+
+        k = k.view(B, T, self.n_heads, C // self.n_heads).transpose(1, 2)
+        q = q.view(B, T, self.n_heads, C // self.n_heads).transpose(1, 2)
+        v = v.view(B, T, self.n_heads, C // self.n_heads).transpose(1, 2)
+
+        att = (q @ k.T(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
+        att = F.softmax(att, dim = -1)
+        y = att @ v
+        y = y.transpose(2, 1).continguous().view(B, T, C)
+
+        y = self.c_proj(y)
+        return y
+
 
 
 class Block(nn.Module):
@@ -51,3 +83,22 @@ class GPT(nn.Module):
         ))
 
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+    def forward(self, idx):
+        B, T = idx.size()
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device)
+        pos_emb = self.transformer.wpe(pos)
+
+        tok_emb = self.transformer.wte(idx)
+        
+        x = tok_emb + pos_emb
+
+        for block in self.transformer.h:
+            x = block(x)
+
+        x = self.transformer.ln_f(x)
+
+        logits = self.lm_head(x) #(B, T, vocab_size)
+
+        return logits
+
