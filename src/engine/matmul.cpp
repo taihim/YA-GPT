344f07edc40251c31e0c0746fcd665fc2bd19ef0
Taimur Ibrahim <tai.him18@gmail.com>
2026-06-16 16:00:22 +0200

    added vector gen and timing

--- src/engine/matmul.cpp
+++ src/engine/matmul.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
+#include <random>
+#include <cblas.h>
 
 // The key intuition is just one sentence:
 // Element (i, j) of the result is the dot product of row i from A and column j from B.
@@ -20,6 +23,10 @@
 // j -> P
 // k-> N
 
+// std::vector<float> sigmoid(std::vector<float> vector) {
+    
+// }
+
 std::vector<float> transpose(const std::vector<float>& matrix, int rows, int cols) {
     std::vector<float> tposed(rows*cols, 0); 
     for(int j = 0; j < cols; j++) {
@@ -30,76 +37,128 @@ std::vector<float> transpose(const std::vector<float>& matrix, int rows, int col
     return tposed;
 }
 
-
-int main() {
-    std::cout << "Matrix Multiplication is nice" << std::endl;
-
-    std::vector<float> mat1 = {1, 2, 3, 4, 5, 6}; // 2x3
-    std::vector<float> mat2 = {7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18}; // 3x4
-    int M = 2, N = 3, P = 4;
-    std::vector<float> mat3 = transpose(mat2, N, P);
+std::vector<float> gen_matrix(const int rows, const int cols) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dist(0.0f, 1.0f);
     
-    std::vector<float> res1(M * P, 0);
-    std::vector<float> res2(M * P, 0);
+    std::vector<float> result(rows*cols, 0);
 
-    // std::vector<float> res = transpose(mat2, N, P);
-    
-    // M = 2 N = 3 P = 4
-    for(int i = 0; i < M; i ++) {
-        for(int j = 0; j < P; j++) {
-            float temp = 0.0;
-            for(int k = 0; k < N; k++) {
-                temp += mat1[i * N + k] * mat2[k * P + j];
-            };
-            res1[i * P + j] = temp;
-        };
-    };
-    
-    // multiply with transposed matrix
-    for(int i = 0; i < M; i ++) {
-        for(int j = 0; j < P; j++) {
-            float temp = 0.0;
-            for(int k = 0; k < N; k++) {
-                temp += mat1[i * N + k] * mat3[j * N + k];
-            };
-            res2[i * P + j] = temp;
-        };
-    };
+    for(float& x: result) x = dist(gen);
 
-
-    for (const auto& x : res1) {
-        std::cout << x << " ";
-    };
-
-    for (const auto& x : res2) {
-        std::cout << x << " ";
-    };
-
-    return 0;
+    return result;
 }
 
 
+int main() {
+    int M = 4, N = 4, P = 4;
+    // std::vector<float> mat1 = {1, 2, 3, 4, 5, 6}; // 2x3
+    // std::vector<float> mat2 = {7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18}; // 3x4
+    
+    // int M = 64, N = 128, P = 64;
+    
+    std::vector<float> mat1 = gen_matrix(M, N);
+    std::vector<float> mat2 = gen_matrix(N, P);
+
+    std::vector<float> res1(M * P, 0);
+    std::vector<float> res2(M * P, 0);
+    std::vector<float> res3(M * P, 0);
 
 
+    auto start1 = std::chrono::steady_clock::now();
+    cblas_sgemm(
+        CblasRowMajor,
+        CblasNoTrans,
+        CblasNoTrans,
+        M,
+        P,
+        N,
+        1.0f,
+        mat1.data(),
+        N,
+        mat2.data(),
+        P,
+        0.0f,
+        res1.data(),
+        P
+    );
+    auto end1 = std::chrono::steady_clock::now();
+    auto ns1 = std::chrono::duration_cast<std::chrono::nanoseconds>(end1 - start1).count();
 
 
+    // took 3320710669 ns for int M = 512, N = 1024, P = 1536;
+    // auto start1 = std::chrono::steady_clock::now();
+    // // M = 2 N = 3 P = 4
+    // for(int i = 0; i < M; i ++) {
+    //     for(int j = 0; j < P; j++) {
+    //         for(int k = 0; k < N; k++) {
+    //             res1[i * P + j] += mat1[i * N + k] * mat2[k * P + j];
+    //         };
+    //     };
+    // };
+    // auto end1 = std::chrono::steady_clock::now();
+    // auto ns1 = std::chrono::duration_cast<std::chrono::nanoseconds>(end1 - start1).count();
+    
 
+    // took 2356132504ns for int M = 512, N = 1024, P = 1536;
+    // auto start2 = std::chrono::steady_clock::now();
+    // std::vector<float> mat3 = transpose(mat2, N, P);
+    // // multiply with transposed matrix
+    // for(int i = 0; i < M; i ++) {
+    //     for(int j = 0; j < P; j++) {
+    //         for(int k = 0; k < N; k++) {
+    //             res2[i * P + j] += mat1[i * N + k] * mat3[j * N + k];
+    //         };
+    //     };
+    // };
+    // auto end2 = std::chrono::steady_clock::now();
+    // auto ns2 = std::chrono::duration_cast<std::chrono::nanoseconds>(end2 - start2).count();
 
+    const int tile_size = 2;
+    auto start3 = std::chrono::steady_clock::now();
+    std::vector<float> mat4 = transpose(mat2, N, P);
+    // tile the matrix
+    for(int i = 0; i < M; i ++) {
+        for(int j = 0; j < P; j++) {
+            for(int k = 0; k < N; k++) {
+                res3[i * P + j] += mat1[i * N + k] * mat4[j * N + k];
+            };
+        };
+    };
+    auto end3 = std::chrono::steady_clock::now();
+    auto ns3 = std::chrono::duration_cast<std::chrono::nanoseconds>(end3 - start3).count();
 
+    // for (const auto& x : res1) {
+    //     std::cout << x << " ";
+    // };
+    // std::cout << std::endl;
+    // std::cout << "Time taken matmul standard: " << ns1 << std::endl;
 
+    // for (const auto& x : res2) {
+    //     std::cout << x << " ";
+    // };
+    
+    std::cout << std::endl;
+    std::cout << "Time taken matmul cblas: " << ns1 << std::endl;
 
+    // std::cout << std::endl;
+    // std::cout << "Time taken matmul transposed: " << ns2 << std::endl;
 
-
-
-
-
-
-
-
-
-
-
-
+    std::cout << std::endl;
+    std::cout << "Time taken matmul transposed + tiled: " << ns3 << std::endl;
+    
+    const auto eps = 1e-5f;
+    for (int i = 0; i < M * P; i++) {
+        if(abs(res1[i] - res3[i]) <= eps){
+            continue;
+        }
+        std::cout << "Results not equal" << std::endl;
+        return 0;
+    };
+    std::cout << "Results correct" << std::endl;
+    
+    return 0;
+}
 
 
 
@@ -150,8 +209,6 @@ int main() {
 
 //0 1 0 1
 //2 3 2 3
-
-
 
 
 // 0 0 0 0 1 1 1 1
