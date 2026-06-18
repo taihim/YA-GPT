f7441f3977a0c7e6e9a7cf5bc1b9216c1e6f9429
Taimur Ibrahim <tai.him18@gmail.com>
2026-06-18 16:43:27 +0200

    implemented tiled matmul

--- src/engine/matmul.cpp
+++ src/engine/matmul.cpp
@@ -51,14 +51,14 @@ std::vector<float> gen_matrix(const int rows, const int cols) {
 
 
 int main() {
-    int M = 4, N = 4, P = 4;
-    std::vector<float> mat1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}; // 4x4
-    std::vector<float> mat2 = {17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32}; // 4x4
+    // int M = 4, N = 4, P = 4;
+    // std::vector<float> mat1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}; // 4x4
+    // std::vector<float> mat2 = {17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32}; // 4x4
     
-    // int M = 64, N = 64, P = 64;
+    int M = 1024, N = 1024, P = 1024;
     
-    // std::vector<float> mat1 = gen_matrix(M, N);
-    // std::vector<float> mat2 = gen_matrix(N, P);
+    std::vector<float> mat1 = gen_matrix(M, N);
+    std::vector<float> mat2 = gen_matrix(N, P);
 
     std::vector<float> res1(M * P, 0);
     std::vector<float> res2(M * P, 0);
@@ -87,17 +87,16 @@ int main() {
 
 
     // took 3320710669 ns for int M = 512, N = 1024, P = 1536;
-    // auto start1 = std::chrono::steady_clock::now();
-    // // M = 2 N = 3 P = 4
-    // for(int i = 0; i < M; i ++) {
-    //     for(int j = 0; j < P; j++) {
-    //         for(int k = 0; k < N; k++) {
-    //             res1[i * P + j] += mat1[i * N + k] * mat2[k * P + j];
-    //         };
-    //     };
-    // };
-    // auto end1 = std::chrono::steady_clock::now();
-    // auto ns1 = std::chrono::duration_cast<std::chrono::nanoseconds>(end1 - start1).count();
+    auto start1 = std::chrono::steady_clock::now();
+    for(int i = 0; i < M; i ++) {
+        for(int j = 0; j < P; j++) {
+            for(int k = 0; k < N; k++) {
+                res1[i * P + j] += mat1[i * N + k] * mat2[k * P + j];
+            };
+        };
+    };
+    auto end1 = std::chrono::steady_clock::now();
+    auto ns1 = std::chrono::duration_cast<std::chrono::nanoseconds>(end1 - start1).count();
     
 
     // took 2356132504ns for int M = 512, N = 1024, P = 1536;
@@ -114,9 +113,8 @@ int main() {
     // auto end2 = std::chrono::steady_clock::now();
     // auto ns2 = std::chrono::duration_cast<std::chrono::nanoseconds>(end2 - start2).count();
 
-    const int tile_size = 2;
+    const int tile_size = 16;
     auto start3 = std::chrono::steady_clock::now();
-    std::vector<float> mat4 = transpose(mat2, N, P);
     // tile the matrix, no transpose
     auto tiledM = M / tile_size;
     auto tiledN = N / tile_size;
@@ -125,12 +123,16 @@ int main() {
     for(int i = 0; i < tiledM; i ++) {
         for(int j = 0; j < tiledP; j++) {
             for(int k = 0; k < tiledN; k++) {
+                // i, j, k iterate through the tiles now instead of through every element in the matrix. for 4x4 matrix with tile_size 2, that means 4 tiles
+                // i gives us the row of a tile in matrix A
+                // j gives us the column of a tile in matrix B
+                // k iterates through A's columns and through B's rows
+
                 // auto tileA = i * tiledN + k;
                 // auto tileB = k * tiledP + j;
-
                 // std::cout << "Tile A:" << tileA << std::endl;
                 // std::cout << "Tile B:" << tileB << std::endl;
-
+    
                 for(int ii = 0; ii < tile_size; ii++) {
                     for(int jj = 0; jj < tile_size; jj++) {
                         for(int kk = 0; kk < tile_size; kk++) {
@@ -138,13 +140,22 @@ int main() {
                             // for row, i * tile_size gives us the row the tile is in and then ii tells us which row within the tile
                             // so i * tile_size + ii -> row of element
                             // for col, k * tile_size + kk -> col of element
-                            std::cout << "A: " << (i*tile_size + ii) * N + (k*tile_size + kk) << "| B: " << " " << std::endl;
-                            // std::cout << "A: " << ( tileA * tile_size ) + ii*tile_size + kk << "| B: " << ((tileB * tile_size) + kk * tile_size + jj) << std::endl;
-                            
+
+                            // for matrix B, j chooses the col and k chooses the row so
+                            // col = j * tile_size + jj
+                            // row = k * tile_size + kk
+                            // index = row * P + col
+
+                            // output will have P columns
+                            // so r * P + c
+                            // output has row of A and col of B
+
+                            // std::cout << "A: " << (i*tile_size + ii) * N + (k*tile_size + kk) << "| B: " << (k*tile_size + kk) * P + (j*tile_size + jj) << std::endl;
+                            res3[(i*tile_size+ii) * P + (j*tile_size+jj)] += mat1[(i*tile_size + ii) * N + (k*tile_size + kk)] * mat2[(k*tile_size + kk) * P + (j*tile_size + jj)];
                         }
                     }
                 }
-                std::cout << "-------------------------------\n";
+                // std::cout << "-------------------------------\n";
             };
         };
     };
@@ -154,8 +165,8 @@ int main() {
     // for (const auto& x : res1) {
     //     std::cout << x << " ";
     // };
-    // std::cout << std::endl;
-    // std::cout << "Time taken matmul standard: " << ns1 << std::endl;
+    std::cout << std::endl;
+    std::cout << "Time taken matmul standard: " << ns1 << std::endl;
 
     // for (const auto& x : res2) {
     //     std::cout << x << " ";
@@ -167,18 +178,18 @@ int main() {
     // std::cout << std::endl;
     // std::cout << "Time taken matmul transposed: " << ns2 << std::endl;
 
-    // std::cout << std::endl;
-    // std::cout << "Time taken matmul transposed + tiled: " << ns3 << std::endl;
+    std::cout << std::endl;
+    std::cout << "Time taken matmul tiled: " << ns3 << std::endl;
     
-    // const auto eps = 1e-5f;
-    // for (int i = 0; i < M * P; i++) {
-    //     if(abs(res1[i] - res3[i]) <= eps){
-    //         continue;
-    //     }
-    //     std::cout << "Results not equal" << std::endl;
-    //     return 0;
-    // };
-    // std::cout << "Results correct" << std::endl;
+    const auto eps = 1e-5f;
+    for (int i = 0; i < M * P; i++) {
+        if(abs(res1[i] - res3[i]) <= eps){
+            continue;
+        }
+        std::cout << "Results not equal" << std::endl;
+        return 0;
+    };
+    std::cout << "Results correct" << std::endl;
     
     return 0;
 }
